2c8d8e4d440a1237bc6419b44e7a4acfdcb1f369
dem4ply <dem4ply@gmail.com>
2019-01-26 23:17:54 -0600

    spawn shotgun

--- Assets/_script/spawner/hack/Spawn_enemy_1.cs
+++ Assets/_script/spawner/hack/Spawn_enemy_1.cs
@@ -3,24 +3,48 @@ using System.Collections.Generic;
 
 namespace spawner
 {
-	public class Spawn_enemy_1: chibi_base.Chibi_behaviour
+	public class Spawn_enemy_1 : chibi_base.Chibi_behaviour
 	{
-		public List<GameObject> objects;
-		public bool is_continuos = false;
-		public float rate_spawn = 1f;
-		public int current = 0;
+		public GameObject objects;
+		public List< GameObject > target;
 
 		public virtual GameObject spawn()
 		{
-			var result = _instance( objects[ current++ ] );
-			if ( current >= objects.Count )
-				current = 0;
+			if ( helper.game_object.comp.is_null( objects ) )
+			{
+				Debug.LogError( "el spawn no tiene object" );
+				return null;
+			}
+			var result = _instance( objects );
 			return result;
 		}
 
 		protected virtual GameObject _instance( GameObject obj )
 		{
-			return helper.instantiate._( obj, transform.position );
+			var collider = GetComponent<BoxCollider>();
+			var max = collider.bounds.max;
+			var min = collider.bounds.min;
+			var position = new Vector3(
+				Random.Range( min.x, max.x ),
+				Random.Range( min.y, max.y ),
+				Random.Range( min.z, max.z )
+			);
+			var result = helper.instantiate._( obj, position );
+			var seek = result.GetComponent<
+				chibi.motor.behavior.steering.Seek >();
+			int pos = new System.Random().Next( 0, target.Count );
+			seek.target = target[ pos ];
+			return result;
+		}
+
+		protected void OnDrawGizmos()
+		{
+			var collider = GetComponent<BoxCollider>();
+			Matrix4x4 rotationMatrix = Matrix4x4.TRS(
+				transform.position, transform.rotation, transform.lossyScale );
+			Gizmos.matrix = rotationMatrix;
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawWireCube( collider.center, collider.size );
 		}
 	}
 }
---
+++ Assets/_script/spawner/hack/Timer_hack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace spawner
+{
+	namespace invoker
+	{
+		public class Timer_hack : chibi.Chibi_behaviour
+		{
+			public Spawn_enemy_1 spawn;
+			public float delta_time = 1f;
+			public int amount_of_spawn = 0;
+
+			protected float _sigma_time = 0f;
+			protected int _amount_of_spawns = 0;
+
+			protected void Update()
+			{
+				_sigma_time += Time.deltaTime;
+				if ( _sigma_time >= delta_time )
+				{
+					spawn.spawn();
+					_sigma_time -= delta_time;
+				}
+			}
+		}
+	}
+}
