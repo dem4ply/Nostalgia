cc259a3dc61543ad666b59f9a3f4553039a16e33
renetronico <47061979+renetronico@users.noreply.github.com>
2019-01-26 22:16:28 -0600

    a torre

---
+++ Assets/_script/giroTorreta.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giroTorreta : MonoBehaviour
+{	
+	public Rigidbody rb;
+	public KeyCode derecha;
+	public KeyCode izquierda;
+	public KeyCode disparar;
+	
+	public GameObject disparo;
+	
+	void FixedUpdate()
+    {
+		//if (colocado en torreta){
+        if(Input.GetKey(derecha)){
+			transform.Rotate(0, Time.deltaTime * 100.0f, 0);
+		}
+		if(Input.GetKey(izquierda)){
+			transform.Rotate(0, Time.deltaTime * -100.0f, 0);
+		}
+		if(Input.GetKeyDown(disparar)){
+			GetComponentInChildren<weapon.gun.Linear_gun>().shot();
+			//disparo.SetActive(true);
+			//disparo.SetActive(false);
+		}
+		//}
+	}
+	void OnTriggerEnter (Collider other) {
+		if(other.gameObject.tag=="Player")
+		{
+			Debug.Log("Colision con torreta");
+		}
+		
+	}
+	
+	
+}
