8e39036d99d0bec82a792fca269355a9985dd269
dem4ply <dem4ply@gmail.com>
2019-01-27 04:59:13 -0600

    dfgzsdfsgs

---
+++ Assets/_script/damage/HP_motor_enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace damage
+{
+	namespace motor
+	{
+		public class HP_motor_enemy : HP_motor
+		{
+			public override void take_damage( Damage damage )
+			{
+				base.take_damage( damage );
+				if ( is_dead )
+					Destroy( gameObject );
+			}
+		}
+	}
+}
